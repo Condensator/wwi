30560c3a7dadc2e2d3df3b190b1e1d42732234e9
Condensator <email>
2021-10-04 11:30:42 +0300

    AdventureWorks

--- Warehouse/Tables/Creation-Scripts/StockGroups_Archive.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE [Warehouse].[StockGroups_Archive](	  [StockGroupID] INT NOT NULL	, [StockGroupName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL	, [LastEditedBy] INT NOT NULL	, [ValidFrom] DATETIME2(7) NOT NULL	, [ValidTo] DATETIME2(7) NOT NULL)GO
