fe7565e8d367004a27eab34efe5414bf68c28474
Condensator <email>
2021-10-19 11:42:06 +0300

    init-4

---
+++ Website/Table Types/Creation-Scripts/OrderLineList.sql
@@ -0,0 +1,12 @@
+CREATE TYPE [Website].[OrderLineList] AS TABLE(
+	[OrderReference] [int] NULL,
+	[StockItemID] [int] NULL,
+	[Description] [nvarchar](100) COLLATE Latin1_General_100_CI_AS NULL,
+	[Quantity] [int] NULL,
+	INDEX [IX_Website_OrderLineList] NONCLUSTERED 
+(
+	[OrderReference] ASC
+)
+)
+WITH ( MEMORY_OPTIMIZED = ON )
+GO
